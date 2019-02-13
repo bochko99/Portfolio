cb3e84bd747f09e9bdc3cf9bf4fa764252a84efe
SkaviCos <skavinski90@mail.ru>
2019-02-13 15:44:03 +0300

    Some changes

--- scripts/test_report.sh
+++ scripts/test_report.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 TARGET_FOLDER="$(date '+%Y-%m-%d_%H:%M:%S')"
-cd C:/Users/Константин/Documents/GitHub/cryptApi
+cd ~/IdeaProjects/cryptApi/
 gradle clean test
 gradle allureReport
 ditto build/reports/allure-report/ reports/$TARGET_FOLDER
