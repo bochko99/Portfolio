c2d21ecafdad70c0dfa950503c3a0aa6cdb3974f
alexander.tolstov <alexander.tolstov@crypterium.com>
2019-02-22 15:26:02 +0300

    test 1

---
+++ scripts/tc_autoInc.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+newVersion=1.0
+curl -v --request PUT -d %%newVersion%% --Header "Content-Type: text/plain" http://username:password@servername:8080/httpAuth/app/rest/projects/%system.teamcity.projectName%/parameters/system.version
