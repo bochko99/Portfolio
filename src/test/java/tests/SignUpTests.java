626dd05db1bb9494b0b2f21b3f0f273b1acb800e
alexander.tolstov <alexander.tolstov@crypterium.com>
2019-06-05 10:09:30 +0300

    new test to check sms code confirmation on prod-like stands

--- src/test/java/tests/SignUpTests.java
+++ src/test/java/tests/SignUpTests.java
@@ -11,10 +11,10 @@ import com.crypterium.cryptApi.utils.ApiCommonFunctions;
 import com.crypterium.cryptApi.utils.EndPoints;
 import core.TestScope;
 import core.annotations.Credentials;
+import core.annotations.ScopeTarget;
 import io.qameta.allure.*;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.hamcrest.Matchers;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import tests.core.ExwalTest;
@@ -28,6 +28,9 @@ import java.util.stream.Collectors;
 import static com.crypterium.cryptApi.Auth.service;
 import static com.crypterium.cryptApi.pojos.wallets.Currency.*;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertThat;
 
 @Epic(TestScope.REGRESS)
 @Feature("Sign up")
@@ -40,6 +43,30 @@ public class SignUpTests extends ExwalTest {
     public void testRegisterNewUser() {
         registerNewUser();
 
+    }
+
+    @Story("SMS on prod validation")
+    @Description("Test checks that confirmation code on prod-like stands is not 1234")
+    @Test
+    @ScopeTarget({ScopeTarget.Stand.PROD, ScopeTarget.Stand.STAGE})
+    @Credentials(creatingNewUser = true)
+    public void testSmsVerification() {
+        String phoneNumber = "";
+        int statusCode;
+        for (int i = 0; i < 10; i++) {
+            //700000 00001-700000 29999
+
+            phoneNumber = "700000" + new Random().nextInt(3) + RandomStringUtils.random(4, false, true);
+            SignUpReq signup = new SignUpReq()
+                    .setPassword("12345a")
+                    .setPhone(phoneNumber);
+            statusCode = given().body(signup).post(EndPoints.mobile_signup).statusCode();
+            if (statusCode == 200) {
+                break;
+            }
+        }
+        String smsCode = ApiCommonFunctions.getSmsCode(phoneNumber, "MOBILE_SIGN_UP");
+        assertThat(smsCode, not(equalTo("1234")));
     }
 
     @Story("User creates his ETH wallets after signing up")
@@ -55,10 +82,10 @@ public class SignUpTests extends ExwalTest {
         List<Currency> totalCurrencies = totalWallets.stream().map(Wallet::getCurrency).collect(Collectors.toList());
         Set<Currency> distinctTotalCurrencies = new HashSet<>(totalCurrencies);
         Assertions.assertAll(
-                () -> Assert.assertThat(currencies.size(), Matchers.equalTo(distinctCurrencies.size())),
-                () -> Assert.assertThat(currencies, Matchers.hasItems(ETH, CRPT, USDC)),
-                () -> Assert.assertThat(totalCurrencies.size(), Matchers.equalTo(distinctTotalCurrencies.size())),
-                () -> Assert.assertThat(totalCurrencies, Matchers.hasItems(ETH, CRPT, USDC))
+                () -> assertThat(currencies.size(), equalTo(distinctCurrencies.size())),
+                () -> assertThat(currencies, Matchers.hasItems(ETH, CRPT, USDC)),
+                () -> assertThat(totalCurrencies.size(), equalTo(distinctTotalCurrencies.size())),
+                () -> assertThat(totalCurrencies, Matchers.hasItems(ETH, CRPT, USDC))
         );
     }
 
